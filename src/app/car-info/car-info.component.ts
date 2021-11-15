cb8fec4c814fcf4004462219fadaa5d69859e98d
Jason Mandabrandja <jasonmandabrandja@gmail.com>
2021-11-15 10:28:24 +0000

    lint add to angular successfully

--- src/app/car-info/car-info.component.ts
+++ src/app/car-info/car-info.component.ts
@@ -1,60 +1,62 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Car } from 'src/model/car';
 import { IAppState } from 'src/state/car.state';
 
 @Component({
   selector: 'app-car-info',
   templateUrl: './car-info.component.html',
-  styleUrls: ['./car-info.component.css']
+  styleUrls: ['./car-info.component.css'],
 })
 export class CarInfoComponent implements OnInit {
 
-  @Input("car") car: any;
-  @Input("index") index: number = 0;
-  constructor(public store: Store<IAppState>) { }
+  @Input() car: any = [];
 
-  ngOnInit(): void {
-    let stage=["Maintenance des puits et installations pétrolières",
+  @Input() index: number = 0;
 
-"Services de forages et complétion des puits",
+  // eslint-disable-next-line no-useless-constructor
+  constructor (public store: Store<IAppState>) { }
 
-"Etudes et maintenance des pipelines",
+  ngOnInit (): void {
+    let stage = ["Maintenance des puits et installations pétrolières",
 
-"Etudes dans le domaine pétrolier",
+      "Services de forages et complétion des puits",
 
-"Conseils technique et juridiques dans le secteur de l’énergie",
+      "Etudes et maintenance des pipelines",
 
-"Etudes et traitements des données pétrolières",
+      "Etudes dans le domaine pétrolier",
 
-"Services logistiques",
+      "Conseils technique et juridiques dans le secteur de l’énergie",
 
-"Maintenance des installations de raffineries",
+      "Etudes et traitements des données pétrolières",
 
-"Maintenance et installations des pipelines",
+      "Services logistiques",
 
-"Etudes et installations des équipements des énergies renouvelables",
+      "Maintenance des installations de raffineries",
 
-"La fabrication, la vente, l'entretien, les réparations, le négoce, et l'implantation de matériel de forage et de production et plus généralement de matériels destinés à toutes installations industrielles",
+      "Maintenance et installations des pipelines",
 
-"L'inspection et la certification des équipements industriels"
-];
-  let str="";
-for(let i=0;i<stage.length;i++){
-  str+="Service "+(i+1)+" => "+stage[i]+"\n";
-}
-console.log(str);
+      "Etudes et installations des équipements des énergies renouvelables",
+
+      "La fabrication, la vente, l'entretien, les réparations, le négoce, et l'implantation de matériel de forage et de production et plus généralement de matériels destinés à toutes installations industrielles",
+
+      "L'inspection et la certification des équipements industriels"
+    ];
+    let str = "";
+    for (let i = 0; i < stage.length; i++) {
+      str += `Service ${i + 1} => ${stage[i]}\n`;
+    }
+    console.log(str);
   }
 
   public close() {
- 
-      // let x=document.getElementById("car-" + this.index);
-      // if (x !== undefined && x !== null)x.style.display='none';
-      this.store.dispatch({
-        type: 'REMOVE_CAR',
-        payload: {
-          index: this.index
-        }
-      });
+
+    // let x=document.getElementById("car-" + this.index);
+    // if (x !== undefined && x !== null)x.style.display='none';
+    this.store.dispatch({
+      type: 'REMOVE_CAR',
+      payload: {
+        index: this.index,
+      },
+    });
   }
 }
